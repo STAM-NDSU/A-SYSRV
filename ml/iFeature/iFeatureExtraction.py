7a9f0a1f5311874398dc1bd2218d5f8534b860d4
yusuf8834 <56593381+yusuf8834@users.noreply.github.com>
2023-12-04 10:40:48 -0600

    large sample error
    - error with large sample fixed by more controls

--- ml/iFeature/iFeatureExtraction.py
+++ ml/iFeature/iFeatureExtraction.py
@@ -3,37 +3,54 @@ import os
 import logging
 import sys
 
-logger = logging.getLogger(__name__) # TODO: why not workign?
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)  # Add a stream handler to output to console
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 def generate_iFeature(input_file, output_directory, descriptors=None):
+    logger.info("Starting iFeature generation")
 
     if descriptors is None:
         descriptors = ["DPC", "CTDC", "CTDT", "CTDD", "CTriad", "GAAC"]
         # PAAC, Moran is not working
+    logger.info(f"Using descriptors: {descriptors}")
 
     # Ensure the output directory exists
     os.makedirs(output_directory, exist_ok=True)
+    logger.info(f"Output directory '{output_directory}' created or already exists")
 
     python_executable = sys.executable
 
     for descriptor in descriptors:
         logger.info(f"Processing descriptor: {descriptor}")
 
-        # command = [python_executable, "./ml/iFeature/iFeature.py", "--file", input_file, "--type", descriptor]
-        command = [python_executable, os.path.join(os.path.dirname(__file__), "iFeature.py"), "--file", input_file, "--type", descriptor]
-        result = subprocess.run(command, capture_output=True, text=True)
+        try:
+            command = [python_executable, os.path.join(os.path.dirname(__file__), "iFeature.py"), "--file", input_file, "--type", descriptor]
+            result = subprocess.run(command, capture_output=True, text=True)
 
-        # Check if the encoding.tsv file exists
-        source_path = os.path.join(os.path.dirname(__file__), "encoding.tsv") 
-        if os.path.exists(source_path):
-            destination_path = os.path.join(output_directory, f"{descriptor}.tsv")
-            os.rename(source_path, destination_path)
-            
-            # Check if the file is empty
-            if os.path.getsize(destination_path) == 0:
-                logger.warning(f"The file {destination_path} is empty.")
+            if result.returncode != 0:
+                logger.error(f"Error processing descriptor {descriptor}")
+                logger.error(f"Standard Output: {result.stdout}")
+                logger.error(f"Standard Error: {result.stderr}")
+                continue
+
+            source_path = os.path.join(os.path.dirname(__file__), "encoding.tsv") 
+            if os.path.exists(source_path):
+                destination_path = os.path.join(output_directory, f"{descriptor}.tsv")
+                os.rename(source_path, destination_path)
+                logger.info(f"File moved to {destination_path}")
+
+                if os.path.getsize(destination_path) == 0:
+                    logger.warning(f"The file {destination_path} is empty.")
+                else:
+                    logger.info(f"Descriptor {descriptor} processed successfully.")
             else:
-                logger.info(f"Descriptor {descriptor} processed successfully.")
-        else:
-            logger.error(f"Failed to process descriptor {descriptor}.")
-            logger.error(f"Error Output: {result.stderr}")
+                logger.error(f"Failed to find the encoding.tsv file for descriptor {descriptor}.")
+        except Exception as e:
+            logger.exception(f"An exception occurred while processing descriptor {descriptor}: {e}")
+
+    logger.info("iFeature generation completed")
